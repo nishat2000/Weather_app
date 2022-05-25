d1e317634e5c4e22e598e745f0d27a14c93f8b32
nishat2000 <nishat.shaikh27@gmail.com>
2022-05-25 20:59:44 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Weather_app';
+export class AppComponent implements OnInit {
+
+  constructor(private WeatherService: WeatherService){
+
+  }
+  ngOnInit(): void {
+    this.WeatherService.getWeatherData('Vadodara').subscribe({
+      next:(response)=>{
+        console.log(response);
+      }
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '880af09ce9msh25e12a7519f353dp1b300ejsn50f2e96acc83',
 };
 
 /*
